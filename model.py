5efd8deda07f83a8208b89af6ed2e5257dc15eed
LyubimovVladislav <grifon7676@gmail.com>
2023-05-11 17:31:30 +0300

    Add reset

--- model.py
+++ model.py
@@ -7,14 +7,22 @@ class Model:
         self.pipe = StableDiffusionPipeline.from_pretrained("Ojimi/anime-kawai-diffusion", safety_checker=None)
         self.pipe = self.pipe.to(device)
         self.i = 0
+        self.generates = False
 
-    def get_image(self, prompt: str, negative_prompt: str, width=512, height=768) -> Image:
+    def get_image(self, prompt, negative_prompt, width=512, height=768):
         return \
             self.pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=28, width=width,
                       height=height).images[0]
 
-    def get_save_image(self, prompt: str, negative_prompt: str, width=512, height=768):
-        filename = f'picture#{self.i}.png'
+    def get_save_image(self, prompt, negative_prompt, width=512, height=768):
+        if not self.generates:
+            return None
+        self.generates = True
+        filename = f'picture_{self.i}.png'
         self.i += 1
         self.get_image(prompt, negative_prompt, width, height).save(filename)
+        self.generates = False
         return filename
+
+    async def manual_reset(self):
+        self.generates = False
